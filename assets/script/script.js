0509ede4adb765cfb8433349ea1d31df0b237177
dimensitiga <112740434+dimensitiga@users.noreply.github.com>
2022-09-03 10:22:23 +0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+$(".carouselx").owlCarousel({
+    margin: 20,
+    loop: false,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+        },
+        600: {
+            items: 2,
+            nav: false,
+        },
+        1000: {
+            items: 2,
+            nav: false,
+        },
+    },
+});
